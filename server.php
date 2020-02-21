f37d47afbc55615b4a15d5f8a35681c72b37f30a
RIC2210 <59483087+RIC2210@users.noreply.github.com>
2020-02-21 13:12:34 +0700

    Create server.php

---
+++ server.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL || ~E_NOTICE);
+header("Content-type:text/html;charset=utf-8");
+include_once("dataserver.php");
+$user=$_POST['user'];
+if($user==$user1){
+echo 'true';
+}else{
+echo 'false';
+}
+ ?>
